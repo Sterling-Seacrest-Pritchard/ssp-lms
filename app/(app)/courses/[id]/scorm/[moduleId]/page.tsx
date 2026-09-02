08edd785c42eebcb4970ccf703819a36ae021f5f
Ian Harrison <iharrison@sspins.com>
2026-09-02 08:52:41 -0400

    feat: add learner-facing SCORM launch route

---
+++ app/(app)/courses/[id]/scorm/[moduleId]/page.tsx
@@ -0,0 +1,34 @@
+import { notFound } from "next/navigation";
+import { auth } from "@/auth";
+import { getScormLaunchInfo } from "@/lib/scorm/launch-info";
+import { ScormPlayer } from "@/components/scorm/scorm-player";
+
+export default async function LearnerScormPage(
+  props: PageProps<"/courses/[id]/scorm/[moduleId]">
+) {
+  const { moduleId } = await props.params;
+
+  const session = await auth();
+  const userId = session?.user?.email;
+  if (!userId) {
+    notFound();
+  }
+
+  const info = await getScormLaunchInfo(moduleId);
+  if (!info) {
+    notFound();
+  }
+
+  const contentUrl = `/api/scorm/content/${moduleId}/${info.launchUrl}`;
+
+  return (
+    <div className="mx-auto flex max-w-4xl flex-col gap-4">
+      <ScormPlayer
+        moduleVersionId={moduleId}
+        contentUrl={contentUrl}
+        scormVersion={info.scormVersion}
+        userId={userId}
+      />
+    </div>
+  );
+}
